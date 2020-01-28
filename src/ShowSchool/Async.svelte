416ffec4fd1134c632b0b0c46318c31aa336aed5
Nicke Broström <54627464+Nickztar@users.noreply.github.com>
2020-01-28 11:51:10 +0100

    home icon for when moved week

--- src/ShowSchool/Async.svelte
+++ src/ShowSchool/Async.svelte
@@ -4,6 +4,8 @@
     import Loading from '../Loading.svelte';
     import Error from '../Error.svelte';
     import { selectedSchool, requestedWeek } from '../stores.js';
+    import TiHome from 'svelte-icons/ti/TiHome.svelte';
+    let displayReturn = false;
     let foodData = getData();
     async function getData(){
         const response = await fetch(`https://matsedlarna.herokuapp.com/${$selectedSchool.value}/${$requestedWeek}`);
@@ -14,15 +16,29 @@
             throw new Error(json);
         }
     }
-    requestedWeek.subscribe(()=>{
+    requestedWeek.subscribe((val)=>{
+        if (val != 0){
+            displayReturn = true;
+        }
+        else{
+            displayReturn = false;
+        }
         foodData = getData();
     });
+    function returnHome(){
+        requestedWeek.set(0);
+    }
 </script>
 
 {#await foodData}
     <Loading/>
 {:then data}
     {#if data.length > 0}
+        {#if displayReturn}
+            <div class="returnIcon" on:click={returnHome}>
+                <TiHome/>
+            </div>
+        {/if}
         <GlideCarousel foodData={data}/>
     {:else}
         <EmptyWeek/>
@@ -30,3 +46,15 @@
 {:catch error}
     <Error/>
 {/await}
+
+<style>
+    .returnIcon{
+        position: absolute;
+        top: 0;
+        left: 1%;
+        width: 5vh;
+        height: 5vh;
+        color: var(--priority);
+        cursor: pointer;
+    }
+</style>
