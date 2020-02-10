dfbab5705b2a10193372c96708e439b97955a035
Nicke Broström <54627464+Nickztar@users.noreply.github.com>
2020-02-10 13:32:06 +0100

    test1

--- src/Settings.svelte
+++ src/Settings.svelte
@@ -140,7 +140,7 @@
         color: var(--light);
     }
     @keyframes spin{
-        0%  {transform: rotate(0deg);}
-        100% {transform: rotate(-360deg);} 
+        0%  {transform: rotate(0deg)}
+        100% {transform: rotate(-360deg)} 
     }
 </style>
