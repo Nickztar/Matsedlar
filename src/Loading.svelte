4912d46d6096b6be08bd77ba2c17226b863dcede
Nicke <nicholas0012@gmail.com>
2020-02-20 22:44:40 +0100

    New loader, kinda working locate function

--- src/Loading.svelte
+++ src/Loading.svelte
@@ -1,40 +1,103 @@
-<div class="lds-ring"><div></div><div></div><div></div><div></div></div>
+<div class="app">
+  <svg class="loading-spinner" viewBox="-24 -24 48 48">
+    <circle class="path" cx="0" cy="0" r="20" fill="none" stroke-width="6"></circle>
+  </svg>
+</div>
 
 <style>
-    .lds-ring {
-        display: inline-block;
-        position: relative;
-        margin: auto;
-        width: 80px;
-        height: 80px;
+    .app {
+        align-items: center;
+        display: flex;
+        height: 100%;
+        justify-content: center;
+        width: 100%;
     }
-    .lds-ring div {
-        box-sizing: border-box;
-        display: block;
-        position: absolute;
-        width: 64px;
-        height: 64px;
-        margin: 8px;
-        border: 8px solid var(--priority);
-        border-radius: 50%;
-        animation: lds-ring 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
-        border-color: var(--priority) transparent transparent transparent;
+    .app .loading-spinner {
+        animation: rotate 2.625s linear infinite;
+        height: 60px;
+        transform-origin: center;
+        width: 70px;
     }
-    .lds-ring div:nth-child(1) {
-        animation-delay: -0.45s;
+    .app .loading-spinner .path {
+        animation: dash 1.5s ease-in-out infinite, color 6s ease-in-out 0.75s infinite;
+        stroke: var(--priority);
+        stroke-linecap: round;
     }
-    .lds-ring div:nth-child(2) {
-        animation-delay: -0.3s;
-    }
-    .lds-ring div:nth-child(3) {
-        animation-delay: -0.15s;
-    }
-    @keyframes lds-ring {
-        0% {
-            transform: rotate(0deg);
-        }
+
+    @-webkit-keyframes rotate {
         100% {
             transform: rotate(360deg);
+        }
+    }
+    @-moz-keyframes rotate {
+        100% {
+            transform: rotate(360deg);
+        }
+    }
+    @-o-keyframes rotate {
+        100% {
+            transform: rotate(360deg);
+        }
+    }
+    @keyframes rotate {
+        100% {
+            transform: rotate(360deg);
+        }
+    }
+    @-webkit-keyframes dash {
+        0% {
+            stroke-dasharray: 0, 150;
+            stroke-dashoffset: 0;
+        }
+        50% {
+            stroke-dasharray: 100, 150;
+            stroke-dashoffset: -24;
+        }
+        100% {
+            stroke-dasharray: 0, 150;
+            stroke-dashoffset: -124;
+        }
+    }
+    @-moz-keyframes dash {
+        0% {
+            stroke-dasharray: 0, 150;
+            stroke-dashoffset: 0;
+        }
+        50% {
+            stroke-dasharray: 100, 150;
+            stroke-dashoffset: -24;
+        }
+        100% {
+            stroke-dasharray: 0, 150;
+            stroke-dashoffset: -124;
+        }
+    }
+    @-o-keyframes dash {
+        0% {
+            stroke-dasharray: 0, 150;
+            stroke-dashoffset: 0;
+        }
+        50% {
+            stroke-dasharray: 100, 150;
+            stroke-dashoffset: -24;
+        }
+        100% {
+            stroke-dasharray: 0, 150;
+            stroke-dashoffset: -124;
+        }
+    }
+    @keyframes dash {
+        0% {
+            stroke-dasharray: 0, 150;
+            stroke-dashoffset: 0;
+        }
+        50% {
+            stroke-dasharray: 100, 150;
+            stroke-dashoffset: -24;
+        }
+        100% {
+            stroke-dasharray: 0, 150;
+            stroke-dashoffset: -124;
         }
     }
 </style>
--- src/SelectSchool/SelectCard.svelte
+++ src/SelectSchool/SelectCard.svelte
@@ -2,13 +2,33 @@
     import FaArrowRight from 'svelte-icons/fa/FaArrowRight.svelte';
     import FaSortAlphaDown from 'svelte-icons/fa/FaSortAlphaDown.svelte';
     import MdLocationOn from 'svelte-icons/md/MdLocationOn.svelte';
+    import Loading from '../Loading.svelte';
     import Select from 'svelte-select';
     import { hasSchool, selectedSchool, showAll } from '../stores.js';
+    let findingSchool = false;
+    let noSchool = false;
     const loadOptions = async (filterText) => {
         const response = await fetch(`https://matsedlarna.herokuapp.com/schools?school=${filterText}`);
         const json = await response.json();
         return json;
     };
+    const closestSchool = async (lat, long) => {
+        if(noSchool) return;
+        const url = `https://api.skolverket.se/planned-educations/school-units?distance=0.5&latitude=${lat}&longitude=${long}&size=1`;
+        const response = await fetch(url, { headers: { "Accept": "application/vnd.skolverket.plannededucations.api.v2.hal+json" }});
+        const data = await response.json();
+        if (data.body.page.totalElements > 0){
+            const name = data.body._embedded.listedSchoolUnits[0].name.replace(/[0-9]/g, '').trim()
+            const school = await loadOptions(name);
+            if (school.length > 0){
+                selectedValue = school[0];
+            }else{
+                noSchool = true;
+            }
+        } else{
+            noSchool = true;
+        }
+    }
     let selectedValue = null;
     function setSchool(){
         if(selectedValue){
@@ -22,11 +42,17 @@
     }
     function findSchool(){
         if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(pos => alert(`${pos.coords.latitude} + ${pos.coords.longitude}`));
+            findingSchool = true;
+            navigator.geolocation.getCurrentPosition(async pos => {
+                await closestSchool(pos.coords.latitude, pos.coords.longitude);
+                findingSchool = false;
+            }, () => findingSchool = false);
         } else { 
+            findingSchool = false;
             console.log('Geolocation is not supported')
         }
     }
+    
     function setCookie(cname, cvalue, exdays) {
         let d = new Date();
         d.setTime(d.getTime() + (exdays*24*60*60*1000));
@@ -34,6 +60,12 @@
         document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
     }
 </script>
+
+{#if findingSchool}
+    <div class="modal">
+        <Loading />
+    </div>
+{/if}
 
 <div class="contain">
     <h1>Hitta din skola</h1>
@@ -44,11 +76,16 @@
         <button id="submit" class="findLoc" on:click={setSchool}><div class="icon"><FaArrowRight /></div></button>
     </div>
     <div class="moreOptions">
-        <button class="allSchools" on:click={findSchool}>Hitta nära skolor</button>
+        <button class="allSchools { noSchool ? 'disable' : ''}" on:click={noSchool ? ()=>{} : findSchool}>Hitta nära skolor</button>
         <button class="allSchools" on:click={allSchools}>Visa alla skolor</button>
-        <button class="allSchoolsIcon" on:click={findSchool}><div class="sortIcon"><MdLocationOn/></div></button>
+        <button class="allSchoolsIcon { noSchool ? 'disable' : ''}" on:click={findSchool}><div class="sortIcon"><MdLocationOn/></div></button>
         <button class="allSchoolsIcon" on:click={allSchools}><div class="sortIcon"><FaSortAlphaDown/></div></button>
     </div>
+    {#if noSchool}
+        <div class="error">
+            <h4>Kunde inte hitta en skola...</h4>
+        </div>
+    {/if}
 </div>
 
 
@@ -56,6 +93,24 @@
     * {
         padding: 0;
         margin: 0;
+        box-sizing: border-box;
+        font-family: 'Roboto', sans-serif;
+    }
+    div .disable{
+        opacity: 0.7;
+        cursor: default;
+    }
+    .modal{
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        top: 0;
+    }
+    .error{
+        text-align: center;
+        padding-top: 5vh;
+        color: var(--priority);
+        opacity: 0.6;
     }
     .moreOptions{
         display: flex;
